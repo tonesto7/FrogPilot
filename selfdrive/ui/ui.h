570bb9d9e4103e0ae25fc504170cea887f2e39a0
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-08 20:02:03 -0700

    Visuals - Custom Themes - Color Theme
    Switch out the standard openpilot color scheme with themed colors.
    
    Want to submit your own color scheme? Post it in the 'feature-request' channel in the FrogPilot Discord!

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -183,6 +183,7 @@ typedef struct UIScene {
   int conditional_speed;
   int conditional_speed_lead;
   int conditional_status;
+  int custom_colors;
   int lead_distance;
   int model_length;
   int steering_angle_deg;
