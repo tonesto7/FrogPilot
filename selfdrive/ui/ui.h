5a8522b57a5622236b939c1619cf9692eb255efc
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-19 20:01:21 -0700

    Visuals - Custom Themes - Turn Signals
    Add themed animation for your turn signals.
    
    Want to submit your own turn signal animation? Post it in the 'feature-request' channel in the FrogPilot Discord!

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -163,6 +163,8 @@ typedef struct UIScene {
   bool tethering_enabled;
   bool traffic_mode;
   bool traffic_mode_active;
+  bool turn_signal_left;
+  bool turn_signal_right;
   bool use_kaofui_icons;
   bool use_vienna_slc_sign;
   bool vtsc_controlling_curve;
@@ -185,6 +187,7 @@ typedef struct UIScene {
   int conditional_status;
   int custom_colors;
   int custom_icons;
+  int custom_signals;
   int lead_distance;
   int model_length;
   int steering_angle_deg;
