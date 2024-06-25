5706af0ac43ee1f856018c2795ff478f18490863
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-24 20:51:34 -0700

    Visuals - Developer UI - Lateral Metrics
    Display various metrics related to the lateral performance of openpilot.

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -137,8 +137,10 @@ typedef struct UIScene {
   bool enabled;
   bool experimental_mode;
   bool experimental_mode_via_screen;
+  bool has_auto_tune;
   bool has_lead;
   bool holiday_themes;
+  bool live_valid;
   bool map_open;
   bool online;
   bool onroad_distance_button;
@@ -161,6 +163,7 @@ typedef struct UIScene {
   bool show_steering;
   bool show_stopping_point;
   bool show_stopping_point_metrics;
+  bool show_tuning;
   bool speed_limit_changed;
   bool speed_limit_controller;
   bool speed_limit_overridden;
@@ -178,9 +181,11 @@ typedef struct UIScene {
   double fps;
 
   float adjusted_cruise;
+  float friction;
   float lane_detection_width;
   float lane_width_left;
   float lane_width_right;
+  float lat_accel;
   float lead_detection_threshold;
   float road_curvature;
   float speed_limit;
