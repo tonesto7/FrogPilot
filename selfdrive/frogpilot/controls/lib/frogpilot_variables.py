a8daa7232f56a89b2aa9f01f6f49e3ba856506a4
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-29 15:05:51 -0700

    FrogPilot setup - FrogPilot Planner

---
+++ selfdrive/frogpilot/controls/lib/frogpilot_variables.py
@@ -0,0 +1,5 @@
+from openpilot.selfdrive.modeld.constants import ModelConstants
+
+CITY_SPEED_LIMIT = 25                   # 55mph is typically the minimum speed for highways
+CRUISING_SPEED = 5                      # Roughly the speed cars go when not touching the gas while in drive
+TRAJECTORY_SIZE = ModelConstants.IDX_N  # Minimum path length
