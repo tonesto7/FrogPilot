3d106340f1085b555abb03fe4895ee49e550ef53
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-07-16 13:02:29 -0700

    July 16th, 2024 Update 2

--- selfdrive/frogpilot/controls/frogpilot_planner.py
+++ selfdrive/frogpilot/controls/frogpilot_planner.py
@@ -25,7 +25,8 @@ A_CRUISE_MIN_SPORT = A_CRUISE_MIN / 2
                   # MPH = [ 0.,  11,  22,  34,  45,  56,  89]
 A_CRUISE_MAX_BP_CUSTOM =  [ 0.,  5., 10., 15., 20., 25., 40.]
 A_CRUISE_MAX_VALS_ECO =   [1.4, 1.2, 1.0, 0.8, 0.6, 0.4, 0.2]
-A_CRUISE_MAX_VALS_SPORT = [4.0, 3.5, 3.0, 1.0, 0.9, 0.8, 0.6]
+A_CRUISE_MAX_VALS_SPORT = [3.0, 2.5, 2.0, 1.0, 0.9, 0.8, 0.6]
+A_CRUISE_MAX_VALS_SPORT_PLUS = [4.0, 3.5, 3.0, 1.0, 0.9, 0.8, 0.6]
 
 TRAFFIC_MODE_BP = [0., CITY_SPEED_LIMIT]
 
@@ -36,6 +37,9 @@ def get_max_accel_eco(v_ego):
 
 def get_max_accel_sport(v_ego):
   return interp(v_ego, A_CRUISE_MAX_BP_CUSTOM, A_CRUISE_MAX_VALS_SPORT)
+
+def get_max_accel_sport_plus(v_ego):
+  return interp(v_ego, A_CRUISE_MAX_BP_CUSTOM, A_CRUISE_MAX_VALS_SPORT_PLUS)
 
 class FrogPilotPlanner:
   def __init__(self):
@@ -130,17 +134,22 @@ class FrogPilotPlanner:
     sport_gear = frogpilotCarState.sportGear
 
     if self.tracking_lead and frogpilot_toggles.aggressive_acceleration:
-      self.max_accel = np.clip(self.lead_one.aLeadK, get_max_accel_sport(v_ego), 2.0 if v_ego >= 20 else 4.0)
+      self.max_accel = np.clip(self.lead_one.aLeadK, get_max_accel_sport_plus(v_ego), 2.0 if v_ego >= 20 else 4.0)
     elif frogpilot_toggles.map_acceleration and (eco_gear or sport_gear):
       if eco_gear:
         self.max_accel = get_max_accel_eco(v_ego)
       else:
-        self.max_accel = get_max_accel_sport(v_ego)
+        if frogpilot_toggles.acceleration_profile == 3:
+          self.max_accel = get_max_accel_sport_plus(v_ego)
+        else:
+          self.max_accel = get_max_accel_sport(v_ego)
     else:
       if frogpilot_toggles.acceleration_profile == 1:
         self.max_accel = get_max_accel_eco(v_ego)
-      elif frogpilot_toggles.acceleration_profile in (2, 3):
-        self.max_accel = get_max_accel_sport(v_ego)
+      elif frogpilot_toggles.acceleration_profile == 2:
+          self.max_accel = get_max_accel_sport(v_ego)
+      elif frogpilot_toggles.acceleration_profile == 3:
+          self.max_accel = get_max_accel_sport_plus(v_ego)
       elif controlsState.experimentalMode:
         self.max_accel = ACCEL_MAX
       else:
